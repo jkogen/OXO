5fe2ef1fc7570fa1db428394964fcc2617b7e5d5
jkogen <jkogen@DESKTOP-QS2D38V>
2022-02-11 00:25:16 +0100

    change to .NET 6

--- OXOConsole/Gamefield.cs
+++ OXOConsole/Gamefield.cs
@@ -1,61 +1,60 @@
-﻿namespace OXO
+﻿namespace OXO;
+
+public class Gamefield
 {
-    public class Gamefield
+    public char?[] Field { get; set; } = new char?[9];
+
+    /// <summary>
+    /// Gibt den Spieler zurück, der das Spiel gewonnen hat.
+    /// Gibt NULL zurück, wenn niemand gewonnen hat.
+    /// </summary>
+    /// <param name="player1"></param>
+    /// <param name="player2"></param>
+    /// <returns></returns>
+    public Player IsWon(Player player1, Player player2)
     {
-        public char?[] Field { get; set; } = new char?[9];
-
-        /// <summary>
-        /// Gibt den Spieler zurück, der das Spiel gewonnen hat.
-        /// Gibt NULL zurück, wenn niemand gewonnen hat.
-        /// </summary>
-        /// <param name="player1"></param>
-        /// <param name="player2"></param>
-        /// <returns></returns>
-        public Player IsWon(Player player1, Player player2)
+        //Waagerechten Zeilen auswerten
+        for (int i = 0; i < 9; i += 3)
         {
-            //Waagerechten Zeilen auswerten
-            for (int i = 0; i < 9; i += 3)
+            if (Field[i] != null && Field[i] == Field[i + 1] && Field[i] == Field[i + 2])
             {
-                if (Field[i] != null && Field[i] == Field[i + 1] && Field[i] == Field[i + 2])
-                {
-                    if (Field[i] == player1.FigureTypeCharaacter)
-                        return player1;
-
-                    return player2;
-                }
-            }
-
-            //Senkrechte Spalten auswerten
-            for (int i = 0; i < 3; i++)
-            {
-                if (Field[i] != null && Field[i] == Field[i + 3] && Field[i] == Field[i + 6])
-                {
-                    if (Field[i] == player1.FigureTypeCharaacter)
-                        return player1;
-
-                    return player2;
-                }
-            }
-
-            //Diagonale 1 auswerten
-            if (Field[0] != null && Field[0] == Field[4] && Field[4] == Field[8])
-            {
-                if (Field[0] == player1.FigureTypeCharaacter)
+                if (Field[i] == player1.FigureTypeCharaacter)
                     return player1;
 
                 return player2;
             }
-
-            //Diagonale 2 auswerten
-            if (Field[2] != null && Field[2] == Field[4] && Field[4] == Field[6])
-            {
-                if (Field[2] == player1.FigureTypeCharaacter)
-                    return player1;
-
-                return player2;
-            }
-
-            return null;
         }
+
+        //Senkrechte Spalten auswerten
+        for (int i = 0; i < 3; i++)
+        {
+            if (Field[i] != null && Field[i] == Field[i + 3] && Field[i] == Field[i + 6])
+            {
+                if (Field[i] == player1.FigureTypeCharaacter)
+                    return player1;
+
+                return player2;
+            }
+        }
+
+        //Diagonale 1 auswerten
+        if (Field[0] != null && Field[0] == Field[4] && Field[4] == Field[8])
+        {
+            if (Field[0] == player1.FigureTypeCharaacter)
+                return player1;
+
+            return player2;
+        }
+
+        //Diagonale 2 auswerten
+        if (Field[2] != null && Field[2] == Field[4] && Field[4] == Field[6])
+        {
+            if (Field[2] == player1.FigureTypeCharaacter)
+                return player1;
+
+            return player2;
+        }
+
+        return null;
     }
 }
--- OXOConsole/Player.cs
+++ OXOConsole/Player.cs
@@ -2,103 +2,101 @@
 using System.Linq;
 using System.Threading;
 
-namespace OXO
+namespace OXO;
+public class Player
 {
-    public class Player
+    public ConsoleColor PlayerColor => FigureType == Figure.X ? ConsoleColor.Green : ConsoleColor.Red;
+
+    public enum Figure { X, O };
+    public Figure FigureType = Figure.X;
+    public char FigureTypeCharaacter => FigureType.ToString()[0];
+    public string Name { get; set; } = "";
+    private bool isBot = false;
+    public bool IsBot
     {
-        public ConsoleColor PlayerColor => FigureType == Figure.X ? ConsoleColor.Green : ConsoleColor.Red;
-
-        public enum Figure { X, O };
-        public Figure FigureType = Figure.X;
-        public char FigureTypeCharaacter => FigureType.ToString()[0];
-        public string Name { get; set; } = "";
-        private bool isBot = false;
-        public bool IsBot
+        get => isBot;
+        set
         {
-            get => isBot;
-            set
-            {
-                if (value == true)
-                    Name = "Bot";
+            if (value == true)
+                Name = "Bot";
 
-                isBot = value;
-            }
+            isBot = value;
         }
-        public int Wins { get; set; } = 0;
+    }
+    public int Wins { get; set; } = 0;
 
-        /// <summary>
-        /// Lässt den aktiven Spieler ein Spielfeld besetzen. 
-        /// Der Bot setzt automatisch
-        /// </summary>
-        /// <param name="gamefield"></param>
-        public void Move(Gamefield gamefield)
+    /// <summary>
+    /// Lässt den aktiven Spieler ein Spielfeld besetzen. 
+    /// Der Bot setzt automatisch
+    /// </summary>
+    /// <param name="gamefield"></param>
+    public void Move(Gamefield gamefield)
+    {
+        int index = -1;
+        int[] allowedIndexes = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        string startMessage = "Besetze ein Feld.";
+        bool validInput = false;
+
+        while (!validInput)
         {
-            int index = -1;
-            int[] allowedIndexes = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-            string startMessage = "Besetze ein Feld.";
-            bool validInput = false;
+            validInput = true;
+            index = -1;
 
-            while (!validInput)
+            if (!IsBot)
             {
-                validInput = true;
-                index = -1;
+                var input = this.DrawPlayerText(startMessage, true);
 
-                if (!IsBot)
+                if (input.Length == 1 && int.TryParse(input[0].ToString(), out _))
                 {
-                    var input = this.DrawPlayerText(startMessage, true);
-
-                    if (input.Length == 1 && int.TryParse(input[0].ToString(), out _))
-                    {
-                        //Gültig eingegebenes Spielfeld wird -1 genommen,
-                        //da das interne Spielfeld-Array bei Index 0 beginnt
-                        index = input[0] - '1';
-                    }
-                }
-                //Bot setzt
-                else
-                {
-                    //Bot nachdenken lassen
-                    for (int i = 0; i < 5; i++)
-                    {
-                        var textPoints = string.Concat(Enumerable.Repeat(".", i));
-                        this.DrawPlayerText(startMessage + textPoints, false);
-                        Thread.Sleep(600);
-                    }
-
-                    //so lange setzen, bis es passt
-                    do
-                    {
-                        Random x = new Random();
-                        index = x.Next(0, 9);
-                    } while (gamefield.Field[index] != null);
-                }
-
-                if (!allowedIndexes.Contains(index))
-                {
-                    startMessage = "Bitte verwende ein gültiges Feld (1-9).";
-                    validInput = false;
-                }
-                else if (gamefield.Field[index] != null)
-                {
-                    startMessage = "Bitte verwende ein freies Feld.";
-                    validInput = false;
-                }
-                else
-                {
-                    startMessage = "Besetze ein Feld.";
+                    //Gültig eingegebenes Spielfeld wird -1 genommen,
+                    //da das interne Spielfeld-Array bei Index 0 beginnt
+                    index = input[0] - '1';
                 }
             }
+            //Bot setzt
+            else
+            {
+                //Bot nachdenken lassen
+                for (int i = 0; i < 5; i++)
+                {
+                    var textPoints = string.Concat(Enumerable.Repeat(".", i));
+                    this.DrawPlayerText(startMessage + textPoints, false);
+                    Thread.Sleep(600);
+                }
 
-            gamefield.Field[index] = FigureTypeCharaacter;
+                //so lange setzen, bis es passt
+                do
+                {
+                    Random x = new Random();
+                    index = x.Next(0, 9);
+                } while (gamefield.Field[index] != null);
+            }
+
+            if (!allowedIndexes.Contains(index))
+            {
+                startMessage = "Bitte verwende ein gültiges Feld (1-9).";
+                validInput = false;
+            }
+            else if (gamefield.Field[index] != null)
+            {
+                startMessage = "Bitte verwende ein freies Feld.";
+                validInput = false;
+            }
+            else
+            {
+                startMessage = "Besetze ein Feld.";
+            }
         }
 
-        /// <summary>
-        /// Setzt den Spielernamen über die Eingabe 
-        /// </summary>
-        public void GetName()
-        {
-            var name = this.DrawPlayerText("Wie ist dein Name? ", true, false);
-            Name = name;
-        }
+        gamefield.Field[index] = FigureTypeCharaacter;
+    }
+
+    /// <summary>
+    /// Setzt den Spielernamen über die Eingabe 
+    /// </summary>
+    public void GetName()
+    {
+        var name = this.DrawPlayerText("Wie ist dein Name? ", true, false);
+        Name = name;
     }
 }
